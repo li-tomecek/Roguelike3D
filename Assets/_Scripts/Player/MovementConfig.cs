de0704217bfabcef6fab806933ae766d146b2fc0
Liana Tomecek <liana.tomecek@gmail.com>
2025-05-13 19:38:46 -0600

    Added acceleration to movement, implemented basic jump, config values on sliders.
    all parameters in the config scriptable object are used. Adjustable jump height and gravity force to simulate jumping characters of varying masses.

--- Assets/_Scripts/Player/MovementConfig.cs
+++ Assets/_Scripts/Player/MovementConfig.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MovementConfig", menuName = "Scriptable Objects/Player/MovementConfig")]
 public class MovementConfig : ScriptableObject
 {
-    public float moveSpeedMin, moveSpeedMax, acceleration, jumpForce;
+    [Range(0.0f, 50.0f)]
+    public float
+        moveSpeedMin,  //base movement speed
+        moveSpeedMax,  //max movement speed
+        acceleration,  //how much the speed increases per second
+        jumpHeight,
+        gravityForce;
+
+    [Range(0.0f, 1.0f)]
+    public float jumpMovementScale;  //how much the lateral movement is scaled when jumping (e.g. 0.5 for half)    
+
+
 }
--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,33 +8,68 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController _controller;
     [SerializeField] private MovementConfig _movementConfig;
 
-    private Vector3 _nextStep;
+    private float _speed;
     private Vector3 _directionalInput;
+    private Vector3 _movementVector;
     private bool _isGrounded;
 
     void Start()
     {
         _controller = gameObject.GetComponent<CharacterController>();
+        IsGrounded();
     }
 
     void Update()
     {
-        //TODO: look into global input handler when you regain access to D2L (instead of handling player input in this script)
+
+        //TODO: look into global input handler and imput maps when you regain access to D2L (instead of handling player input in this script)
         _directionalInput.x = Input.GetAxis("Horizontal");
         _directionalInput.z = Input.GetAxis("Vertical");
 
-        if (_directionalInput == Vector3.zero)
-            return;
+        //Look in the direction of the _directionalAxis vector and adjust the movement speed
+        if (_directionalInput == Vector3.zero) 
+        {
+            _speed = 0f;
+        }
+        else
+        {
+            transform.rotation = Quaternion.LookRotation(_directionalInput.normalized, Vector3.up); 
 
-        //Look in the direction of the _directionalAxis vector
-        transform.rotation = Quaternion.LookRotation(_directionalInput.normalized, Vector3.up);
+            if (_speed == 0)
+                _speed = _movementConfig.moveSpeedMin;
+            else
+                _speed = Mathf.Min(_speed + (_movementConfig.acceleration * Time.deltaTime), _movementConfig.moveSpeedMax);
+
+            _directionalInput *= _speed;
+        }
+    
+
+        // Check for Jump
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _movementVector.y = Mathf.Sqrt(_movementConfig.jumpHeight * 2f * _movementConfig.gravityForce);   //v_0 = sqrt(2 * gravity * height)
+            _isGrounded = false;
+        }
+
+        if (!_isGrounded)
+        {
+            _directionalInput *= _movementConfig.jumpMovementScale;
+            _movementVector.y -= _movementConfig.gravityForce * Time.deltaTime;   // continuously add to gravitational acceleration until unit is grounded
+            
+            if (_movementVector.y < 0 && IsGrounded())  // only check for grounded condition after player starts falling again
+                _movementVector.y = 0f;
+        } 
 
         //Move in desired direction
-        _controller.Move(_directionalInput * _movementConfig.moveSpeedMin * Time.deltaTime);
+        _movementVector.x = _directionalInput.x;
+        _movementVector.z = _directionalInput.z;
+
+        _controller.Move(_movementVector * Time.deltaTime);
     }
 
-    public void Jump()
+    public bool IsGrounded()
     {
-        //TODO
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.3f);
+        return _isGrounded;
     }
 }
