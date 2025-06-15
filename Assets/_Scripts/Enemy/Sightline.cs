1f500e31841786f43ee2ecfcfa9b6a539e829c85
Liana Tomecek <liana.tomecek@gmail.com>
2025-06-14 20:03:55 -0600

    Commbat starts on chasing enemy collision with controlled player
    controlled player is on a separate layer to other player units. raycast and collision detection check for this layermask

--- Assets/_Scripts/Enemy/Sightline.cs
+++ Assets/_Scripts/Enemy/Sightline.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Sightline : MonoBehaviour
 {           
     [SerializeField] private float _sightDistance = 7f; ///how far the sighline raycast will go
-    [SerializeField] private LayerMask _objectMask;
+    [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _chaseSpeed;
     private bool _spottedTarget;
     void Start()
@@ -22,9 +22,18 @@ public class Sightline : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, CombatManager.Instance.GetPlayerUnits()[0].transform.position, _chaseSpeed * Time.deltaTime);
             transform.LookAt(CombatManager.Instance.GetPlayerUnits()[0].transform.position);
         }
-        else if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _sightDistance, _objectMask))
+        else if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _sightDistance, _layerMask))
         {
             _spottedTarget = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!CombatManager.Instance.InCombat() && (1 << other.gameObject.layer) == _layerMask)
+        {
+            Debug.Log("eyooo");
+            CombatManager.Instance.BeginBattle();
         }
     }
 }
