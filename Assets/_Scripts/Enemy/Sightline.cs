f56574052e08a92a494c4b9c7e1b58dc22079232
Liana Tomecek <liana.tomecek@gmail.com>
2025-06-15 20:03:06 -0600

    chasing done through charactercontroller.Move instead of Translate.
    now enemy will collide with obstacles as well. uses a separate raycast (shorter) to determine if they've "hit" the target.
    Also target is not the object the raycast detects and not always the player , so it can be re-used for other purposes.

--- Assets/_Scripts/Enemy/Sightline.cs
+++ Assets/_Scripts/Enemy/Sightline.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Sightline : MonoBehaviour
@@ -5,31 +7,45 @@ public class Sightline : MonoBehaviour
     [SerializeField] private float _sightDistance = 7f; ///how far the sighline raycast will go
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _chaseSpeed;
+    [SerializeField] private float _detectionRadius;
+
+    private CharacterController _controller;
+ 
     private bool _spottedTarget;
+    GameObject target;
+    Vector3 _targetDirection;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
 
     void FixedUpdate()
     {
         if (CombatManager.Instance.InCombat())
             return;
-        
+
         //Debug.DrawRay(transform.position, transform.forward * _sightDistance, Color.red);
-        
-        if (_spottedTarget) //chase the target
+        if (_spottedTarget)
         {
-            transform.position = Vector3.MoveTowards(transform.position, CombatManager.Instance.GetPlayerUnits()[0].transform.position, _chaseSpeed * Time.deltaTime);
-            transform.LookAt(CombatManager.Instance.GetPlayerUnits()[0].transform.position);
-        }
+            if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _detectionRadius, _layerMask))
+            {
+                    CombatManager.Instance.BeginBattle();
+            } else
+            {
+                _targetDirection = (target.transform.position - transform.position).normalized;
+                _targetDirection.y = 0f;
+
+                transform.LookAt(target.transform.position);
+                _controller.Move(_targetDirection * _chaseSpeed * Time.deltaTime);
+            }
+
+        } 
         else if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _sightDistance, _layerMask))
         {
             _spottedTarget = true;
+            target = hit.transform.gameObject;
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!CombatManager.Instance.InCombat() && (1 << other.gameObject.layer) == _layerMask)
-        {
-            CombatManager.Instance.BeginBattle();
-        }
     }
 }
