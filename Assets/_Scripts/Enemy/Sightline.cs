ab307f2db390efb66a3cbe08baae13892fa6efc7
Liana Tomecek <liana.tomecek@gmail.com>
2025-06-15 21:27:31 -0600

    Enemy now patrols around specified nodes (transforms)
    enemy has NavMeshAgent component that it uses to patrol AND CHASE (no longer uses CharacterController.Move).
    Uses spherecast for detection now (instead of raycast)

--- Assets/_Scripts/Enemy/Sightline.cs
+++ Assets/_Scripts/Enemy/Sightline.cs
@@ -1,13 +1,26 @@
-using System.Collections;
-using System.Security.Cryptography;
-using UnityEngine;
 
-public class Sightline : MonoBehaviour
-{           
-    [SerializeField] private float _sightDistance = 7f; ///how far the sighline raycast will go
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Patrol : MonoBehaviour
+{
+    [Header("Target Detection")]
+    [SerializeField] private float _sightDistance = 7f; //how far the sighline raycast will go
+    [SerializeField] private float _collisionDistance = 0.7f;
+    [SerializeField] private float _spherecastRadius = 1f;
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private float _chaseSpeed;
-    [SerializeField] private float _detectionRadius;
+
+    [Header("Patrol")]
+    [SerializeField] private List<Transform> _patrolNodes;
+    [SerializeField] private float _patrolSpeed;
+
+    private NavMeshAgent _agent;
+    private int _nodeIndex = 0;
+
 
     private CharacterController _controller;
  
@@ -17,34 +30,43 @@ public class Sightline : MonoBehaviour
 
     private void Start()
     {
+        _agent = GetComponent<NavMeshAgent>();
         _controller = GetComponent<CharacterController>();
+        _agent.speed = _patrolSpeed;
+
+        if (_patrolNodes.Count > 0)
+            _agent.SetDestination(_patrolNodes[0].position);
     }
 
     void FixedUpdate()
     {
         if (CombatManager.Instance.InCombat())
-            return;
-
-        //Debug.DrawRay(transform.position, transform.forward * _sightDistance, Color.red);
-        if (_spottedTarget)
         {
-            if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _detectionRadius, _layerMask))
+            _agent.enabled = false; //disable the NavMeshAgent   
+            return;
+        }
+
+        if (_spottedTarget) //chasing the target
+        {
+            if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _collisionDistance, _layerMask))
             {
-                    CombatManager.Instance.BeginBattle();
+                CombatManager.Instance.BeginBattle();
             } else
             {
-                _targetDirection = (target.transform.position - transform.position).normalized;
-                _targetDirection.y = 0f;
-
-                transform.LookAt(target.transform.position);
-                _controller.Move(_targetDirection * _chaseSpeed * Time.deltaTime);
+                _agent.SetDestination(target.transform.position);
             }
 
-        } 
-        else if (Physics.Raycast(gameObject.transform.position, transform.forward, out RaycastHit hit, _sightDistance, _layerMask))
+        } //Check for the target
+        else if (Physics.SphereCast(gameObject.transform.position, _spherecastRadius, transform.forward, out RaycastHit hit, _sightDistance, _layerMask))
         {
             _spottedTarget = true;
             target = hit.transform.gameObject;
+            _agent.speed = _chaseSpeed;
+        } 
+        else if(_patrolNodes.Count > 1 && _agent.remainingDistance <= _collisionDistance) //patrol
+        {
+            _nodeIndex = (_nodeIndex + 1) % _patrolNodes.Count;
+            _agent.SetDestination(_patrolNodes[_nodeIndex].position);
         }
 
     }
