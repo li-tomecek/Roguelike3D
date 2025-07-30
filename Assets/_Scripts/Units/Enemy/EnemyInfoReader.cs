81507a6e3f6c3140b8fd156d9c9176a64fbbf4ff
Liana Tomecek <liana.tomecek@gmail.com>
2025-07-30 14:51:02 -0600

    started parsing the csv file data

--- Assets/_Scripts/Units/Enemy/EnemyInfoReader.cs
+++ Assets/_Scripts/Units/Enemy/EnemyInfoReader.cs
@@ -1,10 +1,31 @@
+using System;
 using UnityEngine;
 
 public class EnemyInfoReader : Singleton<EnemyInfoReader>
 {
-    public const string FILE_NAME = "EnemyInfo";
-    private string _fileContents;
+    #region Fields and Properties
+    [Header("Enemy Prefabs")] 
+    [SerializeField] private GameObject _easyPrefab;
+    [SerializeField] private GameObject _mildPrefab;
+    [SerializeField] private GameObject _moderatePrefab;
+    [SerializeField] private GameObject _hardPrefab;
     
+    [Header ("Difficulty Thresholds")]      //ToDo: Either read these values from file, or write to file so they dont both have to be adjustsed
+    [SerializeField] private float _easyThreshold = 0f;
+    [SerializeField] private float _mildThreshold = 0.2f;
+    [SerializeField] private float _moderateThreshold = 0.6f;
+    [SerializeField] private float _hardThreshold = 1f;
+    
+    //Constants -- these change only if csv file itself changes
+    private const string FILE_NAME = "EnemyInfo";
+    private const int NAME_ROW = 1;
+    private const int SKILLS_START_ROW = 6;
+    private const int AI_START_ROW = 8;
+    
+    private string _fileContents;
+    private string[] _rows;
+    
+    #endregion
     public override void Awake()
     {
         TextAsset csvFile = Resources.Load<TextAsset>(FILE_NAME);
@@ -12,7 +33,11 @@ public class EnemyInfoReader : Singleton<EnemyInfoReader>
         if (csvFile != null)            //Read the enemy info file
         {
             _fileContents = csvFile.text;
-            Debug.Log(_fileContents);
+            _rows = _fileContents.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string row in _rows)
+            {
+                Debug.Log(row);
+            }
         }
         else
         {
@@ -21,8 +46,18 @@ public class EnemyInfoReader : Singleton<EnemyInfoReader>
         
     }
 
-    public void ReadEnemyDataFromRow(EnemyUnit unit, int rowIndex)
+    public GameObject CreateEnemyDataFromRow(int rowIndex)
     {
+        var columns = _rows[rowIndex].Split(',');
+        GameObject enemy = Instantiate(_easyPrefab);    //Temp ToDo: separate the different prefabs based on the read difficulty threshold
+        EnemyUnit unitData = enemy.GetComponent<EnemyUnit>();
+        
         //Parse the information of one row and put the read data into the specified enemy unit
+        unitData.name = columns[NAME_ROW];
+        
+        //not done here
+        
+        //disable gameobject and place in combatManager enemy list > this goes in whatever script is creating the object.
+        return enemy;
     }
 }
